ef04acee05eb9f7c4c5303d8d119159102992dfe
Alex Reichert <reichertjalex@gmail.com>
2020-09-10 14:44:10 -0400

    Fix incorrect comment

--- plugins/calculator.js
+++ plugins/calculator.js
@@ -5,7 +5,7 @@ const handleMessageCreated = async (res, message) => {
   const {body, conversation_id} = message;
   const [prefix, ...rest] = body.split(' ');
 
-  // If the message is not from a customer, ignore it
+  // Only execute this if the message starts with '/calculate'
   if (prefix !== '/calculate' && prefix !== 'calculate') {
     return res.json({ok: true});
   }
